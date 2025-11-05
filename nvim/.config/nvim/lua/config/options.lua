8adb47526ff4111c5e5a879271f150bb6ed5c9e1
Rich Snapp <rich.snapp@gmail.com>
2025-11-05 16:42:02 +0000

    just more futzing with the copy/paste in nvim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -7,6 +7,9 @@ local function paste()
   }
 end
 
+local in_tmux = vim.env.TMUX ~= nil
+local paste_fn = in_tmux and require("vim.ui.clipboard.osc52").paste("+") or paste
+
 vim.g.clipboard = {
   name = "OSC 52",
   copy = {
@@ -14,8 +17,8 @@ vim.g.clipboard = {
     ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
   },
   paste = {
-    ["+"] = paste,
-    ["*"] = paste,
+    ["+"] = paste_fn,
+    ["*"] = paste_fn,
   },
 }
 
