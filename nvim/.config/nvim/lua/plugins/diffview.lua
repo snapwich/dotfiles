2d298610617fcc2dbf036985bd385a2b923e8b01
Rich Snapp <rich.snapp@gmail.com>
2025-11-06 22:52:02 +0000

    add leader keys for diffview

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -1,4 +1,9 @@
 return {
   "sindrets/diffview.nvim",
-  opts = {}
+  keys = {
+    { "<leader>dd", "<cmd>DiffviewOpen<cr>", desc = "Diffview: open (working tree changes)" },
+    { "<leader>dh", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview: file history (current)" },
+    { "<leader>dH", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview: file history (all)" },
+  },
+  opts = {},
 }
