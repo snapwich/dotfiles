c71b59d0062d5472bcf409d351041764b3c7d59e
Rich Snapp <rich.snapp@gmail.com>
2025-11-09 11:38:11 -0700

    disable backdrop for flash.nvim

---
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/flash.nvim",
+  opts = {
+    highlight = {
+      backdrop = false,
+    },
+    modes = {
+      char = {
+        highlight = {
+          backdrop = false
+        }
+      }
+    }
+  }
+}
