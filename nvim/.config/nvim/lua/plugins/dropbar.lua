30256ec6d8b8f8ff08cb9b8f0cb76f794902f504
Rich Snapp <rich.snapp@gmail.com>
2025-11-12 23:38:02 +0000

    right-align dropbar

--- nvim/.config/nvim/lua/plugins/dropbar.lua
+++ nvim/.config/nvim/lua/plugins/dropbar.lua
@@ -2,6 +2,10 @@ return {
   "Bekaboo/dropbar.nvim",
   opts = {
     bar = {
+      padding = {
+        left = 1,
+        right = 1,
+      },
       sources = function()
         local sources = require("dropbar.sources")
         return {
@@ -12,9 +16,19 @@ return {
   },
   config = function(_, opts)
     require("dropbar").setup(opts)
+
+    -- Hook to right-align the winbar
+    vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter", "CursorMoved" }, {
+      callback = function()
+        local winbar = vim.wo.winbar
+        if winbar ~= "" and not winbar:match("^%%=") then
+          vim.wo.winbar = "%=" .. winbar -- Right align
+          -- For center: vim.wo.winbar = "%=" .. winbar .. "%="
+        end
+      end,
+    })
+
     local dropbar_api = require('dropbar.api')
     vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
-    vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
-    vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
   end
 }
