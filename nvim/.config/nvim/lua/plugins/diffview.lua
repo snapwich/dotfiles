407e0253a4c51090b4fae60766d81f34ffaa4838
Rich Snapp <rich.snapp@gmail.com>
2025-11-12 19:34:16 -0700

    add <leader>dm to nvim

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -4,6 +4,25 @@ return {
     { "<leader>dd", "<cmd>DiffviewOpen<cr>",          desc = "Diffview: open (working tree changes)" },
     { "<leader>dh", "<cmd>DiffviewFileHistory %<cr>", desc = "Diffview: file history (current)" },
     { "<leader>dH", "<cmd>DiffviewFileHistory<cr>",   desc = "Diffview: file history (all)" },
+    {
+      "<leader>dm",
+      function()
+        -- Check if origin/main exists, otherwise use origin/master
+        local handle = io.popen("git rev-parse --verify origin/main 2>/dev/null")
+        local default_branch = "master"
+
+        if handle then
+          local result = handle:read("*a")
+          handle:close()
+          if result and result ~= "" then
+            default_branch = "main"
+          end
+        end
+
+        vim.cmd("DiffviewOpen origin/" .. default_branch .. "...HEAD")
+      end,
+      desc = "Diffview: branch changes from origin/main or origin/master",
+    },
   },
   opts = {},
   lazy = false,
