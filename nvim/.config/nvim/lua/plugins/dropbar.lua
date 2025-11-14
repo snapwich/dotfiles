f7ebff0a289ba4465edbe792f6f183f55f1d4cf6
Rich Snapp <rich.snapp@gmail.com>
2025-11-14 17:10:11 +0000

    update dropbar.lua for deprecated general

--- nvim/.config/nvim/lua/plugins/dropbar.lua
+++ nvim/.config/nvim/lua/plugins/dropbar.lua
@@ -1,7 +1,7 @@
 return {
   "Bekaboo/dropbar.nvim",
   opts = {
-    general = {
+    bar = {
       -- show more filetypes than default
       enable = function(buf)
         local buftype = vim.bo[buf].buftype
@@ -14,8 +14,6 @@ return {
         end
         return true
       end,
-    },
-    bar = {
       padding = {
         left = 1,
         right = 1,
@@ -28,21 +26,4 @@ return {
       end,
     },
   },
-  config = function(_, opts)
-    require("dropbar").setup(opts)
-
-    -- Hook to right-align the winbar
-    vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter", "CursorMoved" }, {
-      callback = function()
-        local winbar = vim.wo.winbar
-        if winbar ~= "" and not winbar:match("^%%=") then
-          vim.wo.winbar = "%=" .. winbar -- Right align
-          -- For center: vim.wo.winbar = "%=" .. winbar .. "%="
-        end
-      end,
-    })
-
-    local dropbar_api = require('dropbar.api')
-    vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
-  end
 }
