8c9a86d688055694941bd9dbcd03659ef3561e5c
Rich Snapp <rich.snapp@gmail.com>
2025-11-24 12:43:39 -0700

    switch Code Action and Source Action keys that conflict with Diffview

---
+++ nvim/.config/nvim/lua/plugins/lsp-keymaps.lua
@@ -0,0 +1,43 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      -- Apply these keymaps to all LSP servers
+      ["*"] = {
+        keys = {
+          -- Disable default Code Action and Source Action keymaps (conflict with diffview)
+          { "<leader>ca", false },
+          { "<leader>cA", false },
+
+          -- Disable Codelens keymaps (unused)
+          { "<leader>cc", false },
+          { "<leader>cC", false },
+
+          -- Add new Code Action keymap
+          {
+            "<leader>cc",
+            vim.lsp.buf.code_action,
+            desc = "Code Action",
+            mode = { "n", "v" },
+            has = "codeAction",
+          },
+
+          -- Add new Source Action keymap
+          {
+            "<leader>cC",
+            function()
+              vim.lsp.buf.code_action({
+                context = {
+                  only = { "source" },
+                  diagnostics = {},
+                },
+              })
+            end,
+            desc = "Source Action",
+            has = "codeAction",
+          },
+        },
+      },
+    },
+  },
+}
