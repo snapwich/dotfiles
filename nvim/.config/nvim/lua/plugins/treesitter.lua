5e78760a4e12742a5d13271ab5de852b1d52a27b
Rich Snapp <rich.snapp@gmail.com>
2026-01-07 00:01:12 +0000

    keep [C and ]C as class navigators

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,11 +2,12 @@ return {
   "nvim-treesitter/nvim-treesitter-textobjects",
   opts = function(_, opts)
     if opts.move and opts.move.keys then
-      -- these conflicts with jumpto-diffs
+      -- disable [c ]c for diffview jumpto-diffs
       opts.move.keys.goto_next_start["]c"] = nil
-      opts.move.keys.goto_next_end["]C"] = nil
       opts.move.keys.goto_previous_start["[c"] = nil
-      opts.move.keys.goto_previous_end["[C"] = nil
+      -- remap [C ]C to prev/next class start
+      opts.move.keys.goto_previous_start["[C"] = "@class.outer"
+      opts.move.keys.goto_next_start["]C"] = "@class.outer"
     end
     return opts
   end,
