f8c4e3ba4d6facccf78432676a6904b3f120d1bb
Rich Snapp <rich.snapp@gmail.com>
2026-05-22 09:29:07 -0600

    switch blink-cmp to super-tab

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,19 +1,22 @@
 return {
-	"Saghen/blink.cmp",
-	opts = {
-		completion = {
-			ghost_text = {
-				enabled = false,
-			},
-		},
-		sources = {
-			default = { "lsp", "path", "snippets", "buffer", "obsidian-tasks" },
-			providers = {
-				["obsidian-tasks"] = {
-					module = "obsidian-tasks.cmp.source",
-					name = "ObsidianTasks",
-				},
-			},
-		},
-	},
+  "Saghen/blink.cmp",
+  opts = {
+    keymap = {
+      preset = "super-tab",
+    },
+    completion = {
+      ghost_text = {
+        enabled = false,
+      },
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer", "obsidian-tasks" },
+      providers = {
+        ["obsidian-tasks"] = {
+          module = "obsidian-tasks.cmp.source",
+          name = "ObsidianTasks",
+        },
+      },
+    },
+  },
 }
