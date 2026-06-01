d84f5ce69c90ca59e4b6f81fa38786fd38c20f5b
Rich Snapp <rich.snapp@gmail.com>
2026-05-31 21:28:56 -0600

    remove obsidian-tasks dependencies

--- nvim/.config/nvim/lua/plugins/obsidian-tasks.lua
+++ nvim/.config/nvim/lua/plugins/obsidian-tasks.lua
@@ -1,6 +1,5 @@
 return {
-  "snapwich/obsidian-tasks.nvim",
-  -- dir = "/home/dev/repos/obsidian-tasks.nvim/default",
-  dependencies = { "obsidian-nvim/obsidian.nvim", "Saghen/blink.cmp" },
-  opts = {},
+	"snapwich/obsidian-tasks.nvim",
+	-- dir = "/home/dev/repos/obsidian-tasks.nvim/default",
+	opts = {},
 }
