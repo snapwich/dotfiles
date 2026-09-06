999bb69879948623e32c9743cdd5a011538fdead
Rich Snapp <rich.snapp@gmail.com>
2026-09-06 15:19:42 -0600

    add K9S_FEATURE_GATE_NODE_SHELL=true for k9s

---
+++ k9s/.zprofile.d/k9s.sh
@@ -0,0 +1,4 @@
+# k9s gates node shell (`s` on a node) per context, in
+# ~/.config/k9s/clusters/<cluster>/<context>/config.yaml. this env var turns it
+# on globally instead. undocumented, but read by k9s at startup.
+export K9S_FEATURE_GATE_NODE_SHELL=true
