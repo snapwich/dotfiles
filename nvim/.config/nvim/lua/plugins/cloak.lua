c6412bdb9468d023eaeba887ebc97d88ebb65e12
Rich Snapp <rich.snapp@gmail.com>
2026-09-23 10:27:20 -0600

    add CloakPreviewLine and switch CloakToggle to new keymap

--- nvim/.config/nvim/lua/plugins/cloak.lua
+++ nvim/.config/nvim/lua/plugins/cloak.lua
@@ -1,16 +1,17 @@
 return {
-  "laytan/cloak.nvim",
-  event = "BufReadPre",
-  keys = {
-    { "<leader>uk", "<cmd>CloakToggle<cr>", desc = "Toggle Cloak" },
-  },
-  opts = {
-    patterns = {
-      {
-        file_pattern = { ".env*", "*.env", "env.*" },
-        cloak_pattern = "=.+",
-        replace = nil,
-      },
-    },
-  },
+	"laytan/cloak.nvim",
+	event = "BufReadPre",
+	keys = {
+		{ "<leader>uk", "<cmd>CloakPreviewLine<cr>", desc = "Toggle Cloak For Line" },
+		{ "<leader>uK", "<cmd>CloakToggle<cr>", desc = "Toggle Cloak Globally" },
+	},
+	opts = {
+		patterns = {
+			{
+				file_pattern = { ".env*", "*.env", "env.*" },
+				cloak_pattern = "=.+",
+				replace = nil,
+			},
+		},
+	},
 }
